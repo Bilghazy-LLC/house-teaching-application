ae21120b694aa7e3f124ef6522e8fef9b56ef0be
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-06-01 13:46:06 +0000

    Added check for parents' wards

--- android/app/src/main/java/io/codelabs/digitutor/view/fragment/TutorsFragment.java
+++ android/app/src/main/java/io/codelabs/digitutor/view/fragment/TutorsFragment.java
@@ -6,15 +6,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.transition.TransitionManager;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import io.codelabs.digitutor.R;
 import io.codelabs.digitutor.core.base.BaseActivity;
 import io.codelabs.digitutor.core.datasource.remote.FirebaseDataSource;
 import io.codelabs.digitutor.core.util.AsyncCallback;
+import io.codelabs.digitutor.data.BaseUser;
+import io.codelabs.digitutor.data.model.Parent;
 import io.codelabs.digitutor.data.model.Tutor;
 import io.codelabs.digitutor.databinding.FragmentWithListBinding;
 import io.codelabs.digitutor.view.UserActivity;
@@ -57,6 +61,47 @@ public class TutorsFragment extends Fragment {
         binding.grid.setHasFixedSize(true);
         binding.grid.addItemDecoration(new DividerItemDecoration(requireContext(), lm.getOrientation()));
         loadDataFromDatabase();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseDataSource.getUser(requireActivity(), ((BaseActivity) requireActivity()).firestore,
+                ((BaseActivity) requireActivity()).prefs.getKey(), ((BaseActivity) requireActivity()).prefs.getType(), new AsyncCallback<BaseUser>() {
+                    @Override
+                    public void onError(@Nullable String error) {
+
+                    }
+
+                    @Override
+                    public void onSuccess(@Nullable BaseUser response) {
+                        if (response instanceof Parent) {
+                            if (((Parent) response).getWards().isEmpty()) showDialog();
+                        }
+                    }
+
+                    @Override
+                    public void onStart() {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+    }
+
+    private void showDialog() {
+        AlertDialog dialog = new MaterialAlertDialogBuilder(requireActivity()).setTitle("Almost done...")
+                .setMessage("The final step here is for you to add at least one ward. Tap ok to get started.")
+                .setCancelable(false)
+                .create();
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.show();
+
     }
 
     private void loadDataFromDatabase() {
