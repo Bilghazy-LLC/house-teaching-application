065373b4b49c6257b4f48c240379aac7f55fdec8
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-06-02 12:10:53 +0000

    Update TimeTableFragment.kt

--- android/app/src/main/java/io/codelabs/digitutor/view/fragment/TimeTableFragment.kt
+++ android/app/src/main/java/io/codelabs/digitutor/view/fragment/TimeTableFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -139,7 +140,7 @@ class TimeTableFragment : Fragment() {
                             .placeholder(R.drawable.avatar_placeholder)
                             .error(R.drawable.avatar_placeholder)
                             .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
-                            .into(binding.userAvatar)
+                            .into(binding.userAvatar as ImageView)
                     }
                 }
 
